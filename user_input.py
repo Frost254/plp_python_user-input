460f016b0bcb56414fb31c9f431951ff44a9da5f
Derrick Nyasio <dnyasio2014@gmail.com>
2024-02-27 01:12:08 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = str(input("Enter your name surroned in quotes: "))
+age = int(input("Enter your age: "))
+location = str(input("Enter your loaction surrounded in quotes: "))
+
+print("\nHello {}, you are {} years old and live in {}".format(name, age, location))
